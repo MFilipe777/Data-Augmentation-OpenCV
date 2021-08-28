572c27fc308b1bb785bdad2361f318b57237c6e4
Luiz Fernando <62849389+luiz826@users.noreply.github.com>
2021-08-27 21:28:23 -0300

    updating testcv.cpp
    modularizing the code

--- src/testcv.cpp
+++ src/testcv.cpp
@@ -4,21 +4,56 @@
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 
-using namespace std;
-using namespace cv;
+using namespace std; 
+using namespace cv; 
 
+/* Kernel's functions prototypes */
+void applyBlur(Mat imgSrc, Mat imgDst, String imgName);
+void applyBW(Mat imgSrc, Mat imgDst, String imgName);
+void applyCanny(Mat imgSrc, Mat imgDst, String imgName);
+void applyRotate(Mat imgSrc, Mat imgDst, String imgName);
+
+
+/* Main func */
 int main()
 {
-   string path = "me.png";
-   
-   Mat src = imread(path);
-   Mat dst = src.clone();
-   
-   cvtColor(src, dst, CV_BGR2GRAY);
-   Mat dst2;
-   
-   Canny(dst, dst2, 100, 25);
+	string path = "C:\\Users\\luizf\\Documents\\UFPB\\POO\\projeto1\\dataAugmentation\\dataAugmentation\\Lenaaa.jpg";
 
-   imwrite("test.jpg", dst2);
-   return 0;
+	Mat src = imread(path);
+	Mat dst = src.clone();	
+
+	applyCanny(src, dst, "Canny.jpg");
+	applyBW(src, dst, "BW.jpg");
+	applyBlur(src, dst, "Blur.jpg");
+	applyRotate(src, dst, "Rotate.jpg");
+	
+	return 0;
+}
+
+void applyBlur(Mat imgSrc, Mat imgDst, String imgName) 
+{
+	GaussianBlur(imgSrc, imgDst, Size(9, 9), 1.0);
+
+	imwrite(imgName, imgDst);
+}
+
+void applyBW(Mat imgSrc, Mat imgDst, String imgName) 
+{
+	cvtColor(imgSrc, imgDst, COLOR_BGR2GRAY);
+
+	imwrite(imgName, imgDst);
+}
+
+void applyCanny(Mat imgSrc, Mat imgDst, String imgName) 
+{
+	Canny(imgSrc, imgDst, 50, 250);
+
+	imwrite(imgName, imgDst);
+}
+
+void applyRotate(Mat imgSrc, Mat imgDst, String imgName) 
+{
+	flip(imgSrc, imgDst, -1); 
+
+	imwrite(imgName, imgDst);
 }
