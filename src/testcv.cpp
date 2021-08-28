667ac04ebf72681dce5510aab6b140a6b3ddc371
MFilipe777 <79985501+MFilipe777@users.noreply.github.com>
2021-08-28 15:41:17 -0300

    updating testcv.cpp

--- src/testcv.cpp
+++ src/testcv.cpp
@@ -2,58 +2,63 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <string>
 #include <iostream>
 
-using namespace std; 
-using namespace cv; 
+using namespace std;
+using namespace cv;
 
 /* Kernel's functions prototypes */
-void applyBlur(Mat imgSrc, Mat imgDst, String imgName);
-void applyBW(Mat imgSrc, Mat imgDst, String imgName);
-void applyCanny(Mat imgSrc, Mat imgDst, String imgName);
-void applyRotate(Mat imgSrc, Mat imgDst, String imgName);
-
+void applyBlur(Mat imgSrc, Mat imgDst, String imgName, String folderpath);
+void applyBW(Mat imgSrc, Mat imgDst, String imgName, String folderpath);
+void applyCanny(Mat imgSrc, Mat imgDst, String imgName, String folderpath);
+void applyRotate(Mat imgSrc, Mat imgDst, String imgName, String folderpath);
 
 /* Main func */
 int main()
 {
-	string path = "C:\\Users\\luizf\\Documents\\UFPB\\POO\\projeto1\\dataAugmentation\\dataAugmentation\\Lenaaa.jpg";
+	String folderpath = "../dataset/";
+	vector<String> filenames;
+	glob(folderpath, filenames);
 
-	Mat src = imread(path);
-	Mat dst = src.clone();	
+	for (size_t i = 0; i < filenames.size(); i++)
+	{
+		Mat src = imread(filenames[i]);
+		Mat dst = src.clone();
 
-	applyCanny(src, dst, "Canny.jpg");
-	applyBW(src, dst, "BW.jpg");
-	applyBlur(src, dst, "Blur.jpg");
-	applyRotate(src, dst, "Rotate.jpg");
-	
+		applyCanny(src, dst, filenames[i]+"_canny.jpg", folderpath);
+		applyBW(src, dst, filenames[i]+"_bw.jpg", folderpath);
+		applyBlur(src, dst, filenames[i]+"_blur.jpg", folderpath);
+		applyRotate(src, dst, filenames[i]+"_rotate.jpg", folderpath);
+	}
+
 	return 0;
 }
 
-void applyBlur(Mat imgSrc, Mat imgDst, String imgName) 
+void applyBlur(Mat imgSrc, Mat imgDst, String imgName, String folderpath)
 {
 	GaussianBlur(imgSrc, imgDst, Size(9, 9), 1.0);
 
-	imwrite(imgName, imgDst);
+	imwrite(folderpath + imgName, imgDst);
 }
 
-void applyBW(Mat imgSrc, Mat imgDst, String imgName) 
+void applyBW(Mat imgSrc, Mat imgDst, String imgName, String folderpath)
 {
 	cvtColor(imgSrc, imgDst, COLOR_BGR2GRAY);
 
-	imwrite(imgName, imgDst);
+	imwrite(folderpath + imgName, imgDst);
 }
 
-void applyCanny(Mat imgSrc, Mat imgDst, String imgName) 
+void applyCanny(Mat imgSrc, Mat imgDst, String imgName, String folderpath)
 {
 	Canny(imgSrc, imgDst, 50, 250);
 
-	imwrite(imgName, imgDst);
+	imwrite(folderpath + imgName, imgDst);
 }
 
-void applyRotate(Mat imgSrc, Mat imgDst, String imgName) 
+void applyRotate(Mat imgSrc, Mat imgDst, String imgName, String folderpath)
 {
-	flip(imgSrc, imgDst, -1); 
+	flip(imgSrc, imgDst, -1);
 
-	imwrite(imgName, imgDst);
+	imwrite(folderpath + imgName, imgDst);
 }
